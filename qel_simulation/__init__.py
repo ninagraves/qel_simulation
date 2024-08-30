8a51a0af363959beb112a09a7c3e19dd4c07c870
Nina Graves <graves@pads.rwth-aachen.de>
2024-08-30 15:24:52 +0200

    :art: made QuantityGraph ready for import

--- qel_simulation/__init__.py
+++ qel_simulation/__init__.py
@@ -1,5 +1,6 @@
 from qel_simulation.components.quantity_net import QuantityNet
 from qel_simulation.components.quantity_event_log import QuantityEventLog
 from qel_simulation.simulation.simulation import Simulation
+from qel_simulation.components.quantity_net_graph import QuantityGraph
 
 
